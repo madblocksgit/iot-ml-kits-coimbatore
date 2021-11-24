6e7556a22595396551d23a99469fa5217e3fbcb7
MS <72651078+madblocksgit@users.noreply.github.com>
2021-11-24 16:30:52 +0530

    Create experiment3.py

---
+++ experiment3.py
@@ -0,0 +1,13 @@
+# Experiment - 3
+from flask import Flask
+from flask_ngrok import run_with_ngrok
+
+app=Flask(__name__)
+run_with_ngrok(app)
+
+@app.route('/')
+def printHello():
+  return ('Hello World!')
+
+if __name__=="__main__":
+  app.run()
